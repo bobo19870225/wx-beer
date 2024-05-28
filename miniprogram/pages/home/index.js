40f359ead8cea4a26278d3677ae1a80fb77bcc12
bobo19870225 <rain19870225@163.com>
2024-05-28 17:19:39 +0800

    888

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -1,111 +1,176 @@
 const app = getApp()
+const db = wx.cloud.database({
+  env: 'beer-1g75udik38f745cf'
+})
 Component({
-    options: {
-        addGlobalClass: true,
-        multipleSlots: true
-    },
-    data: {
-        orderTotalPrice: null,
-        orderTotalVipPrice: null,
-        orderTotalNumber: null,
-        goodsList: [],
-        orderList: [],
-        showOrder: false,
-        isLoading: false,
-        containerHeight: app.globalData.containerHeight,
-    },
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true
+  },
+  data: {
+    orderTotalPrice: null,
+    orderTotalVipPrice: null,
+    orderTotalNumber: null,
+    goodsList: [],
+    orderList: [],
+    showOrder: false,
+    isLoading: false,
+    containerHeight: app.globalData.containerHeight,
 
-    attached() {
-        console.log("home", "attached")
+    TabCur: 0,
+    MainCur: 0,
+    VerticalNavTop: 0,
+    goodsTypeList: null,
+    load: true
+  },
+
+  attached() {
+    console.log("home", "attached")
+    this.getGoodsTypeList()
+  },
+  ready() {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    getGoodsTypeList() {
+      db.collection('goodsType').get().then((res) => {
+        console.log(res);
+        if (res.data) {
+          this.setData({
+            goodsTypeList: res.data
+          })
+        }
+      })
     },
     /**
-     * 组件的方法列表
+     * 切换店铺
      */
-    methods: {
-        /**
-         * 切换店铺
-         */
-        onShopChange(e) {
-            let shop = e.detail
-            this.fetchGoodsList(shop._id);
-        },
-        async fetchGoodsList(shopId) {
-            this.setData({
-                isLoading: true
-            });
-            const res = await wx.cloud.callFunction({
-                name: 'quickstartFunctions',
-                data: {
-                    type: 'fetchGoodsList',
-                    shopId
-                },
-            });
-            const goodsList = res?.result?.data || [];
-            this.setData({
-                isLoading: false,
-                goodsList
-            });
-        },
-
-        addGoods(e) {
-            let id = e.currentTarget.dataset.index
-            this._handleOrder(id, true)
-        },
-        reduceGoods(e) {
-            let id = e.currentTarget.dataset.index
-            this._handleOrder(id, false)
-        },
-        _handleOrder(goodsId, isAdd = true) {
-            let total = 0
-            let vipTotal = 0
-            let totalNumber = 0
-            let temp = this.data.goodsList.map((item) => {
-                // console.log(goodsId, item._id)
-                if (item._id == goodsId + "") {
-                    let number = item.number
-                    if (isAdd) {
-                        item.number = number ? number + 1 : 1
-                    } else {
-                        item.number = number > 0 ? number - 1 : number
-                    }
-                }
-                return item
-            })
-            let orderTemp = temp.filter((item) => {
-                if (item.number > 0) {
-                    totalNumber += item.number
-                    total += item.number * item.price
-                    vipTotal += item.number * item.vipPrice
-                    return true
-                }
-                return false
-            })
-            // Math.round(）
-            this.setData({
-                goodsList: temp,
-                orderTotalPrice: total / 100,
-                orderTotalVipPrice: vipTotal / 100,
-                orderTotalNumber: totalNumber,
-                orderList: orderTemp
-            });
-        },
-        //关闭弹窗
-        closePopup() {
-            this.setData({
-                showOrder: false
-            })
-        },
-        // 打开弹窗
-        openPopup(e) {
-            this.setData({
-                showOrder: true
-            })
-        },
-        goToPay(e) {
-            let goodsJ = JSON.stringify(this.data.orderList)
-            wx.navigateTo({
-                url: '/pages/pay-car/index?goods=' + goodsJ
-            })
-        }
+    onShopChange(e) {
+      let shop = e.detail
+      this.fetchGoodsList(shop._id);
     },
+    async fetchGoodsList(shopId) {
+      this.setData({
+        isLoading: true
+      });
+      const res = await wx.cloud.callFunction({
+        name: 'quickstartFunctions',
+        data: {
+          type: 'fetchGoodsList',
+          shopId
+        },
+      });
+      const goodsList = res?.result?.data || [];
+      this.setData({
+        isLoading: false,
+        goodsList
+      });
+    },
+
+    addGoods(e) {
+      let id = e.currentTarget.dataset.index
+      this._handleOrder(id, true)
+    },
+    reduceGoods(e) {
+      let id = e.currentTarget.dataset.index
+      this._handleOrder(id, false)
+    },
+    _handleOrder(goodsId, isAdd = true) {
+      let total = 0
+      let vipTotal = 0
+      let totalNumber = 0
+      let temp = this.data.goodsList.map((item) => {
+        // console.log(goodsId, item._id)
+        if (item._id == goodsId + "") {
+          let number = item.number
+          if (isAdd) {
+            item.number = number ? number + 1 : 1
+          } else {
+            item.number = number > 0 ? number - 1 : number
+          }
+        }
+        return item
+      })
+      let orderTemp = temp.filter((item) => {
+        if (item.number > 0) {
+          totalNumber += item.number
+          total += item.number * item.price
+          vipTotal += item.number * item.vipPrice
+          return true
+        }
+        return false
+      })
+      // Math.round(）
+      this.setData({
+        goodsList: temp,
+        orderTotalPrice: total / 100,
+        orderTotalVipPrice: vipTotal / 100,
+        orderTotalNumber: totalNumber,
+        orderList: orderTemp
+      });
+    },
+    //关闭弹窗
+    closePopup() {
+      this.setData({
+        showOrder: false
+      })
+    },
+    // 打开弹窗
+    openPopup(e) {
+      this.setData({
+        showOrder: true
+      })
+    },
+    goToPay(e) {
+      let goodsJ = JSON.stringify(this.data.orderList)
+      wx.navigateTo({
+        url: '/pages/pay-car/index?goods=' + goodsJ
+      })
+    },
+    tabSelect(e) {
+      // console.log(e.currentTarget.dataset.id);
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        MainCur: e.currentTarget.dataset.id,
+        VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+      })
+    },
+    VerticalMain(e) {
+      let that = this;
+      let list = this.data.goodsTypeList;
+      let tabHeight = 0;
+      if (this.data.load) {
+        for (let i = 0; i < list.length; i++) {
+          let view = this.createSelectorQuery().select("#main-" + list[i].value);
+          console.log(view);
+          view.fields({
+            size: true
+          }, data => {
+            console.log(data);
+            list[i].top = tabHeight;
+            tabHeight = tabHeight + data.height;
+            list[i].bottom = tabHeight;
+          }).exec();
+        }
+        that.setData({
+          load: false,
+          goodsTypeList: list
+        })
+      }
+      let scrollTop = e.detail.scrollTop + 20;
+      for (let i = 0; i < list.length; i++) {
+        if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+          that.setData({
+            VerticalNavTop: (list[i].value - 1) * 50,
+            TabCur: list[i].value
+          })
+          return false
+        }
+      }
+    }
+  },
 })
--- miniprogram/pages/manage/dishes-edit/index.js
+++ miniprogram/pages/manage/dishes-edit/index.js
@@ -1,184 +1,218 @@
 // pages/manage/dishes-edit/index.js
 const app = getApp()
 const db = wx.cloud.database({
-    env: 'beer-1g75udik38f745cf'
+  env: 'beer-1g75udik38f745cf'
 })
 
 Page({
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        imgList: [],
-        id: null,
-        shop: null,
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgList: [],
+    goodsTypeList: [],
+    id: null,
+    shop: null,
+    isLoading: false,
+    goods: null,
+    eventChannel: null,
+    index: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.getGoodsTypeList()
+    this.setData({
+      eventChannel: this.getOpenerEventChannel()
+    })
+    const eventChannel = this.data.eventChannel
+    eventChannel && eventChannel.on && eventChannel.on('acceptDataFromOpenerPage', (data) => {
+      let {
+        id,
+        shop
+      } = data
+      this.setData({
+        id,
+        shop
+      })
+      if (id) {
+        this.getGoods(id)
+      }
+    })
+  },
+  getGoodsTypeList() {
+    db.collection('goodsType').get().then((res) => {
+      console.log(res);
+      if (res.data) {
+        this.setData({
+          goodsTypeList: res.data,
+          index: 0
+        })
+      }
+    })
+  },
+  bindPickerChange(e) {
+    const index = e.detail.value
+    this.setData({
+      index
+    })
+  },
+  getGoods(id) {
+    this.setData({
+      isLoading: true
+    })
+    db.collection('goods').doc(id).get().then((res) => {
+      // console.log(res);
+      this.setData({
+        goods: res.data,
+        index:res.data.classify,
         isLoading: false,
-        goods: null,
-        eventChannel: null
-    },
+        imgList: [res.data.img]
+      })
+      this.setData({
+        isLoading: false
+      })
+    })
+  },
+  onUploadOk(e) {
+    // console.log("SC", e);
+    this.setData({
+      imgList: e.detail
+    })
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-        this.setData({
-            eventChannel: this.getOpenerEventChannel()
-        })
-        const eventChannel = this.data.eventChannel
-        eventChannel && eventChannel.on && eventChannel.on('acceptDataFromOpenerPage', (data) => {
-            let {
-                id,
-                shop
-            } = data
-            this.setData({
-                id,
-                shop
-            })
-            if (id) {
-                this.getGoods(id)
-            }
-        })
-    },
-    getGoods(id) {
-        this.setData({
-            isLoading: true
-        })
-        db.collection('goods').doc(id).get().then((res) => {
-            // console.log(res);
-            this.setData({
-                goods: res.data,
-                isLoading: false,
-                imgList:[res.data.img]
-            })
-            this.setData({
-                
-            })
-        })
-    },
-    onUploadOk(e) {
-        // console.log("SC", e);
-        this.setData({
-            imgList: e.detail
-        })
-    },
-
-    formSubmit(e) {
-        let {
-            title,
-            remarks,
-            price,
-            vipPrice
-        } = e.detail.value
-        let img = this.data.imgList[0]
-        const shopId = this.data.shop._id
-        if (!title) {
-            wx.showToast({
-                title: '名称必填',
-                icon: 'error'
-            })
-            return
-        }
-        if (!price || !vipPrice) {
-            wx.showToast({
-                title: '价格必填',
-                icon: 'error'
-            })
-            return
-        }
-        if (!img) {
-            wx.showToast({
-                title: '请上传一张商品图片',
-                icon: 'error'
-            })
-            return
-        }
-        if (!shopId) {
-            wx.showToast({
-                title: '无店铺',
-                icon: 'error'
-            })
-            return
-        }
-        wx.cloud.callFunction({
-            name: 'quickstartFunctions',
-            data: {
-                type: 'updateGoods',
-                id: this.data.id,
-                data: {
-                    title,
-                    remarks,
-                    price,
-                    vipPrice,
-                    shopId,
-                    img,
-                    isDelete: false
-                }
-            },
-        }).then((res) => {
-            wx.navigateBack()
-        });
-    },
-
-    formReset(e) {
-        console.log('form发生了reset事件，携带数据为：', e.detail.value)
-    },
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-        if (app.globalData.selectMap) {
-            let {
-                latitude,
-                longitude
-            } = app.globalData.selectMap
-            this.setData({
-                location: '[' + latitude + ']' + '[' + latitude + ']',
-                latitude,
-                longitude
-            })
-        }
-        app.globalData.selectMap = null
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+  formSubmit(e) {
+    // console.log(e);
+    let {
+      title,
+      remarks,
+      price,
+      vipPrice,
+      classify
+    } = e.detail.value
+    let img = this.data.imgList[0]
+    const shopId = this.data.shop._id
+    if (!title) {
+      wx.showToast({
+        title: '名称必填',
+        icon: 'error'
+      })
+      return
     }
+    if (!price || !vipPrice) {
+      wx.showToast({
+        title: '价格必填',
+        icon: 'error'
+      })
+      return
+    }
+    price = Number.parseInt(price)
+    vipPrice = Number.parseInt(vipPrice)
+    if (!img) {
+      wx.showToast({
+        title: '请上传一张商品图片',
+        icon: 'error'
+      })
+      return
+    }
+    if (!shopId) {
+      wx.showToast({
+        title: '无店铺',
+        icon: 'error'
+      })
+      return
+    }
+    if (classify == null || classify == undefined) {
+      wx.showToast({
+        title: '请选择分类',
+        icon: 'error'
+      })
+      return
+    }
+    classify = Number.parseInt(classify)
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      data: {
+        type: 'updateGoods',
+        id: this.data.id,
+        data: {
+          title,
+          remarks,
+          price,
+          vipPrice,
+          shopId,
+          img,
+          classify,
+          isDelete: false
+        }
+      },
+    }).then((res) => {
+      wx.navigateBack()
+    });
+  },
+
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    if (app.globalData.selectMap) {
+      let {
+        latitude,
+        longitude
+      } = app.globalData.selectMap
+      this.setData({
+        location: '[' + latitude + ']' + '[' + latitude + ']',
+        latitude,
+        longitude
+      })
+    }
+    app.globalData.selectMap = null
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
