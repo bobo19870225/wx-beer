6c7bdcdb85167472cb02bbc6dc92d802f0257066
bobo19870225 <rain19870225@163.com>
2024-05-31 18:26:15 +0800

    3333

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -1,190 +1,190 @@
 const app = getApp()
 const db = wx.cloud.database({
-    env: 'beer-1g75udik38f745cf'
+  env: 'beer-1g75udik38f745cf'
 })
 Component({
-    options: {
-        addGlobalClass: true,
-        multipleSlots: true
-    },
-    data: {
-        orderTotalPrice: null,
-        orderTotalVipPrice: null,
-        orderTotalNumber: null,
-        goodsList: [],
-        orderList: [],
-        showOrder: false,
-        isLoading: false,
-        isRefreshing: false,
-        containerHeight: app.globalData.containerHeight,
-        TabCur: 0,
-        MainCur: 0,
-        VerticalNavTop: 0,
-        goodsTypeList: null,
-        load: true,
-        shop: null
-    },
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true
+  },
+  data: {
+    orderTotalPrice: null,
+    orderTotalVipPrice: null,
+    orderTotalNumber: null,
+    goodsList: [],
+    orderList: [],
+    showOrder: false,
+    isLoading: false,
+    isRefreshing: false,
+    containerHeight: app.globalData.containerHeight,
+    TabCur: 0,
+    MainCur: 0,
+    VerticalNavTop: 0,
+    goodsTypeList: null,
+    load: true,
+    shop: null
+  },
 
-    attached() {
-        console.log("home", "attached")
-        this.getGoodsTypeList()
-    },
-    ready() {
+  attached() {
+    console.log("home", "attached")
+    this.getGoodsTypeList()
+  },
+  ready() {
 
-    },
+  },
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        getGoodsTypeList() {
-            this.setData({
-                isLoading: true
-            })
-            db.collection('goodsType').where({
-                isDelete: false
-            }).orderBy('value', 'asc').get().then((res) => {
-                if (res.data) {
-                    res.data.unshift({
-                        title: '人气Top5',
-                        value: 0
-                    })
-                    this.setData({
-                        isLoading: false,
-                        goodsTypeList: res.data
-                    })
-                }
-            })
-        },
-        /**
-         * 切换店铺
-         */
-        onShopChange(e) {
-            let shop = e.detail
-            this.setData({
-                shop
-            })
-            this.fetchGoodsList();
-        },
-        async fetchGoodsList() {
-            const shopId = this.data.shop._id
-            this.setData({
-                isRefreshing: true
-            });
-            const res = await wx.cloud.callFunction({
-                name: 'quickstartFunctions',
-                data: {
-                    type: 'fetchGoodsList',
-                    shopId
-                },
-            });
-            const goodsList = res?.result?.data || [];
-            this.setData({
-                // isLoading: false,
-                isRefreshing: false,
-                goodsList
-            });
-        },
-
-        addGoods(e) {
-            let id = e.currentTarget.dataset.index
-            this._handleOrder(id, true)
-        },
-        reduceGoods(e) {
-            let id = e.currentTarget.dataset.index
-            this._handleOrder(id, false)
-        },
-        _handleOrder(goodsId, isAdd = true) {
-            let total = 0
-            let vipTotal = 0
-            let totalNumber = 0
-            let temp = this.data.goodsList.map((item) => {
-                // console.log(goodsId, item._id)
-                if (item._id == goodsId + "") {
-                    let number = item.number
-                    if (isAdd) {
-                        item.number = number ? number + 1 : 1
-                    } else {
-                        item.number = number > 0 ? number - 1 : number
-                    }
-                }
-                return item
-            })
-            let orderTemp = temp.filter((item) => {
-                if (item.number > 0) {
-                    totalNumber += item.number
-                    total += item.number * item.price
-                    vipTotal += item.number * item.vipPrice
-                    return true
-                }
-                return false
-            })
-            // Math.round(）
-            this.setData({
-                goodsList: temp,
-                orderTotalPrice: total / 100,
-                orderTotalVipPrice: vipTotal / 100,
-                orderTotalNumber: totalNumber,
-                orderList: orderTemp
-            });
-        },
-        //关闭弹窗
-        closePopup() {
-            this.setData({
-                showOrder: false
-            })
-        },
-        // 打开弹窗
-        openPopup(e) {
-            this.setData({
-                showOrder: true
-            })
-        },
-        goToPay(e) {
-            let goodsJ = JSON.stringify(this.data.orderList)
-            wx.navigateTo({
-                url: '/pages/pay-car/index?goods=' + goodsJ
-            })
-        },
-        tabSelect(e) {
-            // console.log(e.currentTarget.dataset.id);
-            this.setData({
-                TabCur: e.currentTarget.dataset.id,
-                MainCur: e.currentTarget.dataset.id,
-                VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
-            })
-        },
-        VerticalMain(e) {
-            console.log(e);
-            let that = this;
-            let list = this.data.goodsTypeList;
-            let tabHeight = 0;
-            if (this.data.load) {
-                for (let i = 0; i < list.length; i++) {
-                    let view = this.createSelectorQuery().select("#main-" + list[i].value);
-                    view.fields({
-                        size: true
-                    }, data => {
-                        list[i].top = tabHeight;
-                        tabHeight = tabHeight + data.height;
-                        list[i].bottom = tabHeight;
-                    }).exec();
-                }
-                that.setData({
-                    load: false,
-                    goodsTypeList: list
-                })
-            }
-            let scrollTop = e.detail.scrollTop + 20;
-            for (let i = 0; i < list.length; i++) {
-                if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
-                    that.setData({
-                        VerticalNavTop: (list[i].value - 1) * 50,
-                        TabCur: list[i].value
-                    })
-                    return false
-                }
-            }
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    getGoodsTypeList() {
+      this.setData({
+        isLoading: true
+      })
+      db.collection('goodsType').where({
+        isDelete: false
+      }).orderBy('value', 'asc').get().then((res) => {
+        if (res.data) {
+          res.data.unshift({
+            title: '人气Top5',
+            value: 0
+          })
+          this.setData({
+            isLoading: false,
+            goodsTypeList: res.data
+          })
         }
+      })
     },
+    /**
+     * 切换店铺
+     */
+    onShopChange(e) {
+      let shop = e.detail
+      this.setData({
+        shop,
+        goodsList: []
+      })
+      this.fetchGoodsList();
+    },
+    async fetchGoodsList() {
+      const shopId = this.data.shop._id
+      this.setData({
+        isRefreshing: true
+      });
+      const res = await wx.cloud.callFunction({
+        name: 'quickstartFunctions',
+        data: {
+          type: 'fetchGoodsList',
+          shopId
+        },
+      });
+      const goodsList = res?.result?.data || [];
+      this.setData({
+        // isLoading: false,
+        isRefreshing: false,
+        goodsList
+      });
+    },
+
+    addGoods(e) {
+      let id = e.currentTarget.dataset.index
+      this._handleOrder(id, true)
+    },
+    reduceGoods(e) {
+      let id = e.currentTarget.dataset.index
+      this._handleOrder(id, false)
+    },
+    _handleOrder(goodsId, isAdd = true) {
+      let total = 0
+      let vipTotal = 0
+      let totalNumber = 0
+      let temp = this.data.goodsList.map((item) => {
+        // console.log(goodsId, item._id)
+        if (item._id == goodsId + "") {
+          let number = item.number
+          if (isAdd) {
+            item.number = number ? number + 1 : 1
+          } else {
+            item.number = number > 0 ? number - 1 : number
+          }
+        }
+        return item
+      })
+      let orderTemp = temp.filter((item) => {
+        if (item.number > 0) {
+          totalNumber += item.number
+          total += item.number * item.price
+          vipTotal += item.number * item.vipPrice
+          return true
+        }
+        return false
+      })
+      // Math.round(）
+      this.setData({
+        goodsList: temp,
+        orderTotalPrice: total / 100,
+        orderTotalVipPrice: vipTotal / 100,
+        orderTotalNumber: totalNumber,
+        orderList: orderTemp
+      });
+    },
+    //关闭弹窗
+    closePopup() {
+      this.setData({
+        showOrder: false
+      })
+    },
+    // 打开弹窗
+    openPopup(e) {
+      this.setData({
+        showOrder: true
+      })
+    },
+    goToPay(e) {
+      let goodsJ = JSON.stringify(this.data.orderList)
+      wx.navigateTo({
+        url: '/pages/pay-car/index?goods=' + goodsJ
+      })
+    },
+    tabSelect(e) {
+      // console.log(e.currentTarget.dataset.id);
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        MainCur: e.currentTarget.dataset.id,
+        VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+      })
+    },
+    VerticalMain(e) {
+      let that = this;
+      let list = this.data.goodsTypeList;
+      let tabHeight = 0;
+      if (this.data.load) {
+        for (let i = 0; i < list.length; i++) {
+          let view = this.createSelectorQuery().select("#main-" + list[i].value);
+          view.fields({
+            size: true
+          }, data => {
+            list[i].top = tabHeight;
+            tabHeight = tabHeight + data.height;
+            list[i].bottom = tabHeight;
+          }).exec();
+        }
+        that.setData({
+          load: false,
+          goodsTypeList: list
+        })
+      }
+      let scrollTop = e.detail.scrollTop + 20;
+      for (let i = 0; i < list.length; i++) {
+        if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+          that.setData({
+            VerticalNavTop: (list[i].value - 1) * 50,
+            TabCur: list[i].value
+          })
+          return false
+        }
+      }
+    }
+  },
 })
--- miniprogram/pages/map/index.js
+++ miniprogram/pages/map/index.js
@@ -2,150 +2,204 @@
 const app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        latitude: null,
-        longitude: null,
-        shopList: null,
-        eventChannel: null,
-        markers: [],
-        containerHeight: app.globalData.containerHeightNoTabBar,
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    latitude: null,
+    longitude: null,
+    shopList: null,
+    eventChannel: null,
+    markers: [],
+    indexShop: null,
+    containerHeight: app.globalData.containerHeightNoTabBar,
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-        this.setData({
-            eventChannel: this.getOpenerEventChannel()
-        })
-        this.initShopList()
-    },
-    onMapLoaded(e) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.setData({
+      eventChannel: this.getOpenerEventChannel()
+    })
+    this.initShopList()
+  },
+  onMapLoaded(e) {
 
-    },
-    /**
-     * 初始化商店
-     */
-    async initShopList() {
-        const shopList = app.globalData.shopList || await app.getShopList()
-        // let shop = app.globalData.shop
-        this.setData({
-            shopList,
-        })
-        this.loadMarkers()
-    },
-    loadMarkers: function () {
-        const shopList = this.data.shopList
-        let markersData = this.data.markers
-        if (shopList) {
-            shopList.forEach((value, index) => {
-                let markers = {
-                    id: index,
-                    longitude: value.longitude,
-                    latitude: value.latitude,
-                    iconPath: '../../images/icons/shop-location.png',
-                    callout: {
-                        content: value.name,
-                        color: '#13227a',
-                        display: 'ALWAYS',
-                        padding: 6,
-                        borderRadius: 3
-                    },
-                    width: 30,
-                    height: 30,
-                }
-                markersData.push(markers)
-            })
+  },
+  /**
+   * 初始化商店
+   */
+  async initShopList() {
+    const shopList = app.globalData.shopList || await app.getShopList()
+    let shop = app.globalData.shop
+    let index = 0
+    if (shop) {
+      shopList.forEach((element, shopIndex) => {
+        if (shop._id == element._id) {
+          index = shopIndex
         }
-        this.setData({
-            markers: markersData
-        });
-    },
-
-    onSelect(e) {
-        const index = e.detail.markerId
-        const shop = this.data.shopList[index]
-        if (this.data.eventChannel && this.data.eventChannel.emit) {
-            this.data.eventChannel.emit(
-                /** 事件名称 */
-                'callbackData',
-                /** 事件参数 */
-                shop
-            )
-        }
-        wx.navigateBack()
-    },
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-        const mapContext = wx.createMapContext('mapId', this)
-        console.log("onMapLoaded", mapContext);
-        mapContext.moveToLocation({
-            success: (res) => {
-                console.log("moveToLocation", res);
-                mapContext.getCenterLocation({
-                    success: (res) => {
-                        console.log("getCenterLocation", res);
-                        let {
-                            latitude,
-                            longitude
-                        } = res
-                        this.setData({
-                            latitude,
-                            longitude
-                        })
-                    }
-                })
-            },
-            fail: (e) => {
-                console.log("moveToLocation", e);
-            }
-        })
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+      });
+    } else {
+      shop = shopList[index]
     }
+    this.setData({
+      shopList,
+      indexShop: index
+    })
+    this.loadMarkers()
+  },
+  loadMarkers: function () {
+    const shopList = this.data.shopList
+    let markersData = this.data.markers
+    if (shopList) {
+      shopList.forEach((value, index) => {
+        let markers = {
+          id: index,
+          longitude: value.longitude,
+          latitude: value.latitude,
+          iconPath: '../../images/icons/shop-location.png',
+          callout: {
+            content: value.name,
+            color: '#13227a',
+            display: 'ALWAYS',
+            padding: 6,
+            borderRadius: 3
+          },
+          width: 30,
+          height: 30,
+        }
+        markersData.push(markers)
+      })
+    }
+    this.setData({
+      markers: markersData
+    });
+    const shop = this.data.shopList[this.data.indexShop]
+    const mapContext = wx.createMapContext('mapId', this)
+    mapContext.moveToLocation({
+      latitude: shop.latitude,
+      longitude: shop.longitude,
+      success: (res) => {
+        console.log("moveToLocation", res);
+      },
+      fail: (e) => {
+        console.log("moveToLocation", e);
+      }
+    })
+  },
+
+  onMapSelect(e) {
+    const index = e.detail.markerId
+    const shop = this.data.shopList[index]
+    if (this.data.eventChannel && this.data.eventChannel.emit) {
+      this.data.eventChannel.emit(
+        /** 事件名称 */
+        'callbackData',
+        /** 事件参数 */
+        shop
+      )
+    }
+    wx.navigateBack()
+  },
+  onSelect(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index
+    this.setData({
+      indexShop: index
+    })
+    const shop = this.data.shopList[index]
+    const mapContext = wx.createMapContext('mapId', this)
+    mapContext.moveToLocation({
+      latitude: shop.latitude,
+      longitude: shop.longitude,
+      success: (res) => {
+        console.log("moveToLocation", res);
+      },
+      fail: (e) => {
+        console.log("moveToLocation", e);
+      }
+    })
+  },
+  onSure(e) {
+    const shop = this.data.shopList[this.data.indexShop]
+    if (this.data.eventChannel && this.data.eventChannel.emit) {
+      this.data.eventChannel.emit(
+        /** 事件名称 */
+        'callbackData',
+        /** 事件参数 */
+        shop
+      )
+    }
+    wx.navigateBack()
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    // const mapContext = wx.createMapContext('mapId', this)
+    // mapContext.moveToLocation({
+    //     success: (res) => {
+    //         console.log("moveToLocation", res);
+    //         mapContext.getCenterLocation({
+    //             success: (res) => {
+    //                 console.log("getCenterLocation", res);
+    //                 let {
+    //                     latitude,
+    //                     longitude
+    //                 } = res
+    //                 this.setData({
+    //                     latitude,
+    //                     longitude
+    //                 })
+    //             }
+    //         })
+    //     },
+    //     fail: (e) => {
+    //         console.log("moveToLocation", e);
+    //     }
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
