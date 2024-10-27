5241f8477171a8f2a47e8848b4b8e5cd071296fe
摩诘 <oncwnuCW8M2C3AzVaUO1IzlEjDUw@git.weixin.qq.com>
2024-10-27 21:11:25 +0800

    买买买

--- cloudfunctions/quickstartFunctions/payForVip/index.js
+++ cloudfunctions/quickstartFunctions/payForVip/index.js
@@ -1,94 +1,109 @@
 const cloud = require('wx-server-sdk');
 cloud.init({
-  env: cloud.DYNAMIC_CURRENT_ENV
+    env: cloud.DYNAMIC_CURRENT_ENV
 });
 const db = cloud.database({
-  throwOnNotFound: true,
+    throwOnNotFound: true,
 });
 exports.main = async (event, context) => {
-  try {
-    const _ = db.command
-    const entity = event.entity
-    const _openid = cloud.getWXContext().OPENID
-    const name = entity.name
-    const shopId = entity.shopId
-    const entry = entity.entry
-    const price = entity.price
-    const coupon = entity.coupon
-    const vipPackageId = entity.vipPackageId
-    if (!_openid || !shopId || !entry || !coupon || !price || !vipPackageId) {
-      return {
-        success: false,
-        errMsg: '参数不完整:' + JSON.stringify(entity)
-      };
-    }
-    const vipRes = await db.collection('vip').where({
-      _openid,
-      shopId
-    }).get()
-    const vipData = vipRes.data
-    let vipId = null
-    if (vipData && vipData.length > 0) {
-      vipId = vipRes.data[0]._id
-    }
-    const result = await db.runTransaction(async transaction => {
-      let res = null
-      if (vipId) {
-        res = await transaction.collection('vip').doc(vipId).update({
-          data: {
-            name,
-            account: {
-              recharge: _.inc(price),
-              balance: _.inc(entry),
-              coupon: coupon,
-            },
-            updateDate: Date.now(),
-            isDelete: false
-          }
-        })
-      } else {
-        res = transaction.collection('vip').add({
-          data: {
-            _openid,
-            name,
-            shopId,
-            account: {
-              recharge: price,
-              balance: entry,
-              coupon: coupon,
-            },
-            createDate: Date.now(),
-            isDelete: false
-          }
-        })
-      }
-      if (res) {
-        const billAdd = await transaction.collection('bill').add({
-          data: {
-            _openid,
-            createDate: new Date(),
-            remarks: 'vip充值',
-            vipPackageId,
-            money: price,
-            shopId,
-            type: 0,
-          }
-        })
-        if (!billAdd) {
-          await transaction.rollback(-100)
+    try {
+        const _ = db.command
+        const entity = event.entity
+        const _openid = cloud.getWXContext().OPENID
+        const name = entity.name
+        const shopId = entity.shopId
+        const entry = entity.entry
+        const price = entity.price
+        const coupon = entity.coupon
+        const vipPackageId = entity.vipPackageId
+        if (!_openid || !shopId || !entry || !coupon || !price || !vipPackageId) {
+            return {
+                success: false,
+                errMsg: '参数不完整:' + JSON.stringify(entity)
+            };
         }
-      } else {
-        await transaction.rollback(-100)
-      }
-    })
-    return {
-      success: true,
-      msg: result,
+        const vipRes = await db.collection('vip').where({
+            _openid,
+            shopId
+        }).get()
+        const vipData = vipRes.data
+        let vipId = null
+        let vip = null
+        if (vipData && vipData.length > 0) {
+            vip = vipRes.data[0]
+            vipId = vipRes.data[0]._id
+        }
+        const result = await db.runTransaction(async transaction => {
+            let res = null
+            if (vipId) {
+                let isBuyFans = vip.isBuyFans || false
+                if (vipPackageId == 'f3805610671ce09a1108123668cd317d') {
+                    if (isBuyFans) { //粉丝会员只能买一次
+                        await transaction.rollback(-100)
+                    }
+                    isBuyFans = true
+                }
+                res = await transaction.collection('vip').doc(vipId).update({
+                    data: {
+                        name,
+                        account: {
+                            recharge: _.inc(price),
+                            balance: _.inc(entry),
+                            coupon: coupon,
+                        },
+                        updateDate: Date.now(),
+                        isDelete: false,
+                        isBuyFans
+                    }
+                })
+            } else {
+                let isBuyFans = false
+                if (vipPackageId == 'f3805610671ce09a1108123668cd317d') { //粉丝会员只能买一次
+                    isBuyFans = true
+                }
+                res = transaction.collection('vip').add({
+                    data: {
+                        _openid,
+                        name,
+                        shopId,
+                        account: {
+                            recharge: price,
+                            balance: entry,
+                            coupon: coupon,
+                        },
+                        createDate: Date.now(),
+                        isDelete: false,
+                        isBuyFans
+                    }
+                })
+            }
+            if (res) {
+                const billAdd = await transaction.collection('bill').add({
+                    data: {
+                        _openid,
+                        createDate: new Date(),
+                        remarks: 'vip充值',
+                        vipPackageId,
+                        money: price,
+                        shopId,
+                        type: 0,
+                    }
+                })
+                if (!billAdd) {
+                    await transaction.rollback(-100)
+                }
+            } else {
+                await transaction.rollback(-100)
+            }
+        })
+        return {
+            success: true,
+            msg: result,
+        }
+    } catch (e) {
+        return {
+            success: false,
+            errMsg: JSON.stringify(e)
+        }
     }
-  } catch (e) {
-    return {
-      success: false,
-      errMsg: JSON.stringify(e)
-    }
-  }
 };
--- cloudfunctions/wxpayFunctions/wxpay_order/index.js
+++ cloudfunctions/wxpayFunctions/wxpay_order/index.js
@@ -3,44 +3,44 @@
  */
 const cloud = require('wx-server-sdk');
 cloud.init({
-  env: cloud.DYNAMIC_CURRENT_ENV
+    env: cloud.DYNAMIC_CURRENT_ENV
 });
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();
+    const wxContext = cloud.getWXContext();
 
-  // 商户自行生成商户订单号，此处仅为代码示例
-  const outTradeNo = Math.round(Math.random() * 10 ** 13) + Date.now();
+    // 商户自行生成商户订单号，此处仅为代码示例
+    const outTradeNo = Math.round(Math.random() * 10 ** 13) + Date.now();
 
-  // 商户存储订单号到数据库，便于后续与微信侧订单号关联。例如使用云开发云存储能力：
-  // db.collection('orders').add({ data: { outTradeNo } });
-  const description = event.data.description || '<商品描述>'
-  const total = event.data.total
-  if (!total) {
-    return {
-      success: false,
-      errMsg: '金额异常'
-    };
-  }
-  const res = await cloud.callFunction({
-    name: 'cloudbase_module',
-    data: {
-      name: 'wxpay_order',
-      data: {
-        description,
-        amount: {
-          total: total * 100, // 订单金额
-          currency: 'CNY',
+    // 商户存储订单号到数据库，便于后续与微信侧订单号关联。例如使用云开发云存储能力：
+    // db.collection('orders').add({ data: { outTradeNo } });
+    const description = event.data.description || '<商品描述>'
+    const total = event.data.total
+    if (!total) {
+        return {
+            success: false,
+            errMsg: '金额异常'
+        };
+    }
+    const res = await cloud.callFunction({
+        name: 'cloudbase_module',
+        data: {
+            name: 'wxpay_order',
+            data: {
+                description,
+                amount: {
+                    total: total * 100, // 订单金额
+                    currency: 'CNY',
+                },
+                // 商户生成的订单号
+                out_trade_no: outTradeNo,
+                payer: {
+                    // 服务端云函数中直接获取当前用户openId
+                    openid: wxContext.OPENID,
+                },
+            },
         },
-        // 商户生成的订单号
-        out_trade_no: outTradeNo,
-        payer: {
-          // 服务端云函数中直接获取当前用户openId
-          openid: wxContext.OPENID,
-        },
-      },
-    },
-  });
-  return res.result;
+    });
+    return res.result;
 };
